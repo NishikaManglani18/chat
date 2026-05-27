60e1740cf93a1c1e59ded239f052ca60c8f14ed7
NishikaManglani18 <nmanglani18@gmail.com>
2026-05-27 16:41:30 +0530

    Update api.js
    connected railway backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://chat-production-e9e5.up.railway.app",
   withCredentials: true,
 });
 
